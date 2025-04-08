28537502e162d7cfe6d8eee79a8503dac1f06109
Michal-Naron <michanaron@gmail.com>
2025-04-08 12:10:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command == "":
+        return
+    if len(command.split()) != 3:
+        return
+    cp, file, file_copy = command.split()
+    if cp != "cp":
+        return
+    if file == file_copy:
+        return
+    try:
+        with open(file, "r") as file1, open(file_copy, "w") as file_copy1:
+            old_file = "".join(file1.readlines())
+            file_copy1.write(old_file)
+    except FileNotFoundError:
+        return
